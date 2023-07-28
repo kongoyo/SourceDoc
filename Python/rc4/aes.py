530749a7ad8609d51a6838d2d2228877773ebcc8
kongoyo <kongoyo@gmail.com>
2023-07-28 15:45:10 +0800

    modified:   Python/rc4/.vscode/settings.json 	modified:   Python/rc4/aes.py

--- Python/rc4/aes.py
+++ Python/rc4/aes.py
@@ -6,18 +6,18 @@ import pbkdf2
 import pyaes
 
 # Derive a 256-bit AES encryption key from the password
-password = input("Please input password(8-10 chars minimal length is recommended): ")
+password_encrypt = input("Please input password(8-10 chars minimal length is recommended): ")
 passwordSalt = os.urandom(128)
-key1 = pbkdf2.PBKDF2(password, passwordSalt).read(256)
-print('AES encryption key:', binascii.hexlify(key1))
+key_value = pbkdf2.PBKDF2(password_encrypt, passwordSalt).read(256)
+print("AES encryption key:", binascii.hexlify(key_value))
 
 # AES Encryption (CTR Block Mode)
 # Encrypt the plaintext with the given key:
 #   ciphertext = AES-256-CTR-Encrypt(plaintext, key, iv)
-password = input("Please input password: ")
+password_encrypt = input("Please input password: ")
 iv = secrets.randbits(128)
-key = pbkdf2.PBKDF2(password, passwordSalt).read(32)
-plaintext = "Text for encryption"
-aes = pyaes.AESModeOfOperationCTR(key, pyaes.Counter(iv))
-ciphertext = aes.encrypt(plaintext)
-print('Encrypted:', binascii.hexlify(ciphertext))
+key_encrypt = pbkdf2.PBKDF2(password_encrypt, passwordSalt).read(64)
+# plaintext = "Text for encryption"
+aes = pyaes.AESModeOfOperationCTR(key_encrypt, pyaes.Counter(iv))
+ciphertext = aes.encrypt(password_encrypt)
+print("Encrypted:", binascii.hexlify(ciphertext))
