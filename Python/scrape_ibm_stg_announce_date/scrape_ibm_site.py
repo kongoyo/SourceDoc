d0f9330b86aec61504622e92fc79ab1410d0cedc
kongoyo <kongoyo@gmail.com>
2023-08-15 23:10:20 +0800

    modified:   Python/scrape_ibm_stg_announce_date/scrape_ibm_site.py

--- Python/scrape_ibm_stg_announce_date/scrape_ibm_site.py
+++ Python/scrape_ibm_stg_announce_date/scrape_ibm_site.py
@@ -1,20 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.ibm.com/storage/products"
-response = requests.get(url)
+model = input("model: ")
+url = (
+    "https://www.ibm.com/docs/api/v1/content/M"
+    + model
+    + "?announcement=M"
+    + model
+    + "&parsebody=true&lang=en&role="
+)
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, "html.parser")
+html_content = requests.get(url)
+# 解析HTML内容
+soup = BeautifulSoup(html_content.content, "html.parser")
 
-    storage_products = soup.find_all("div", class_="product-card")
+# 找到表头
+thead = soup.find("thead")
+header_cells = thead.find_all("th")
+header_labels = [
+    cell.find("div", class_="bx--table-header-label").text.strip()
+    for cell in header_cells
+]
 
-    for product in storage_products:
-        product_name = product.find("h3", class_="product-card__name").text.strip()
-        announced_date = product.find(
-            "p", class_="product-card__releasedate"
-        ).text.strip()
+# 找到数据行
+tbody = soup.find("tbody")
+data_rows = tbody.find_all("tr")
 
-        print(f"Product: {product_name}\nAnnounced Date: {announced_date}\n")
-else:
-    print("Failed to retrieve data from the IBM website.")
+# 提取数据
+data = []
+for row in data_rows:
+    cells = row.find_all("td")
+    row_data = [cell.text.strip() for cell in cells]
+    data.append(row_data)
+
+# 輸出表格資訊
+print(
+    f"{header_labels[0]:<15} {header_labels[1]:<15} {header_labels[2]:<15} {header_labels[3]:<25} {header_labels[4]:<25}"
+)
+print("=" * 100)
+for row in data:
+    print(f"{row[0]:<15} {row[1]:<15} {row[2]:<15} {row[3]:<25} {row[4]:<25}")
