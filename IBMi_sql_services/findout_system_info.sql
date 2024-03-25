43c8e78b5b6758c305747749e7c290f5a21c76cb
kongoyo <kongoyo@email.com>
2024-03-25 11:13:00 +0800

    modified:   IBMi_sql_services/findout_system_info.sql

--- IBMi_sql_services/findout_system_info.sql
+++ IBMi_sql_services/findout_system_info.sql
@@ -2,4 +2,4 @@
 select * from qsys2.system_status_info;
 
 --- 列出主機型號、序號、名稱、CPU配置、屬性、記憶體配置、硬碟空間配置等資訊
-select machine_model, machine_type, machine_serial_number, host_name, CPU_SHARING_ATTRIBUTE, CURRENT_CPU_CAPACITY, MAIN_STORAGE_SIZE/1024/1024 as MAIN_STORAGE_SIZE_GB, SYSTEM_ASP_STORAGE/1024 as SYSTEM_ASP_STORAGE_GB from qsys2.system_status_info;
+select machine_model, machine_type, machine_serial_number, host_name, CPU_SHARING_ATTRIBUTE, CURRENT_CPU_CAPACITY, MAIN_STORAGE_SIZE/1000/1000 as MAIN_STORAGE_SIZE_GB, SYSTEM_ASP_STORAGE/1000 as SYSTEM_ASP_STORAGE_GB from qsys2.system_status_info;
