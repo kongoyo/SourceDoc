132d0cabbf9cb130052bafe6b098a6580ac09fa3
kongoyo <kongoyo@gmail.com>
2024-05-21 00:41:15 +0800

    modified:   .vscode/launch.json 	modified:   .vscode/settings.json 	new file:   Java/Contact_Management_Tool/contact_tool/.gitignore 	new file:   Java/Contact_Management_Tool/contact_tool/.mvn/wrapper/maven-wrapper.jar 	new file:   Java/Contact_Management_Tool/contact_tool/.mvn/wrapper/maven-wrapper.properties 	new file:   Java/Contact_Management_Tool/contact_tool/mvnw 	new file:   Java/Contact_Management_Tool/contact_tool/mvnw.cmd 	new file:   Java/Contact_Management_Tool/contact_tool/pom.xml 	new file:   Java/Contact_Management_Tool/contact_tool/src/main/java/com/contact/contact_tool/ContactToolApplication.java 	new file:   Java/Contact_Management_Tool/contact_tool/src/main/resources/application.properties 	new file:   Java/Contact_Management_Tool/contact_tool/src/test/java/com/contact/contact_tool/ContactToolApplicationTests.java 	new file:   Java/Contact_Management_Tool/main.java

---
+++ Java/Contact_Management_Tool/contact_tool/src/test/java/com/contact/contact_tool/ContactToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contact.contact_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
