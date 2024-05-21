20c33e2a6740f7b33a4675f6f345478a6aa65048
kongoyo <kongoyo@gmail.com>
2024-05-21 13:48:54 +0800

    modified:   .vscode/launch.json 	new file:   Java/Connect_ibmi/conn_ibmi/.vscode/settings.json 	new file:   Java/Connect_ibmi/conn_ibmi/README.md 	new file:   Java/Connect_ibmi/conn_ibmi/bin/ibmiconn.class 	new file:   Java/Connect_ibmi/conn_ibmi/src/ibmiconn.java 	new file:   Java/Connect_ibmi/ibmiconn.java

---
+++ Java/Connect_ibmi/conn_ibmi/src/ibmiconn.java
@@ -0,0 +1,25 @@
+import java.sql.*;
+
+public class ibmiconn {
+    public static void main(String[] args) throws Exception {
+        try {
+            System.out.println("Trying to connect...");
+            String DRIVER = "com.ibm.as400.access.AS400JDBCDriver";
+            String URL = "jdbc:as400://172.16.14.61";
+            Class.forName(DRIVER);
+            Connection conn = DriverManager.getConnection(URL,"QSECOFR", "PASSWORD");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select AMBSYSN, AMBLIB, AMBFILE, AMBNAME, AMBNRCD, BMBSYSN, BMBLIB, BMBFILE, BMBNAME, BMBNRCD from DDSCINFO.CMPPFMBR");
+            System.out.println("Connected with " + conn);
+         // System.out.println("employee_code, employee_name, monthly_salary");
+            while(rs.next()) {
+                System.out.println(rs.getString("AMBSYSN")+" "+rs.getString("AMBLIB")+" "+rs.getString("BMBSYSN")+" "+rs.getString("BMBLIB"));
+           }
+           conn.close();
+           System.exit(0);
+        }
+        catch(Exception e) {
+           System.out.println(e);
+        }
+    }
+}
