f8770946cac60e213cc1d906808353e7b0d2745a
kongoyo <kongoyo@email.com>
2024-06-27 03:16:15 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   IBMi/QCLSRC.srcpf/CHKTCPIFC.cmd

--- IBMi/QCLSRC.srcpf/CHKTCPIFC.cmd
+++ IBMi/QCLSRC.srcpf/CHKTCPIFC.cmd
@@ -1,4 +1,4 @@
              CMD        PROMPT('Check TCP/IP Interface Status')
 
              PARM       KWD(NETIFC) TYPE(*CHAR) LEN(15) MIN(1) +
-                          PROMPT('Network interface address') 
+                          PROMPT('Network interface address')
