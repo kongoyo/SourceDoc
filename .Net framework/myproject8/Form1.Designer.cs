6274bc96cf1bbba79f0a166fff408641d6c7d1a8
kongoyo <kongoyo@gmail.com>
2024-08-27 18:16:21 +0800

    myproject8_char_userpw

--- .Net framework/myproject8/Form1.Designer.cs
+++ .Net framework/myproject8/Form1.Designer.cs
@@ -30,19 +30,19 @@
         {
             button1 = new System.Windows.Forms.Button();
             label1 = new System.Windows.Forms.Label();
-            label2 = new System.Windows.Forms.Label();
+            cmdstr = new System.Windows.Forms.Label();
             textBox1 = new System.Windows.Forms.TextBox();
-            textBox2 = new System.Windows.Forms.TextBox();
-            textBox3 = new System.Windows.Forms.TextBox();
             label3 = new System.Windows.Forms.Label();
-            textBox4 = new System.Windows.Forms.TextBox();
             label4 = new System.Windows.Forms.Label();
+            systemip = new System.Windows.Forms.MaskedTextBox();
+            userid = new System.Windows.Forms.MaskedTextBox();
+            userpw = new System.Windows.Forms.MaskedTextBox();
             SuspendLayout();
             // 
             // button1
             // 
             button1.Location = new System.Drawing.Point(527, 28);
-            button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            button1.Margin = new System.Windows.Forms.Padding(2);
             button1.Name = "button1";
             button1.Size = new System.Drawing.Size(92, 55);
             button1.TabIndex = 0;
@@ -62,45 +62,25 @@
             label1.Text = "System IP";
             label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label2
+            // cmdstr
             // 
-            label2.AutoSize = true;
-            label2.Location = new System.Drawing.Point(81, 135);
-            label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            label2.Name = "label2";
-            label2.Size = new System.Drawing.Size(115, 15);
-            label2.TabIndex = 2;
-            label2.Text = "cmd messages~!!!!";
+            cmdstr.AutoSize = true;
+            cmdstr.Location = new System.Drawing.Point(81, 135);
+            cmdstr.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            cmdstr.Name = "cmdstr";
+            cmdstr.Size = new System.Drawing.Size(115, 15);
+            cmdstr.TabIndex = 2;
+            cmdstr.Text = "cmd messages~!!!!";
             // 
             // textBox1
             // 
             textBox1.Location = new System.Drawing.Point(81, 174);
-            textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            textBox1.Margin = new System.Windows.Forms.Padding(2);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
             textBox1.Size = new System.Drawing.Size(239, 88);
             textBox1.TabIndex = 3;
             textBox1.Text = "Output something here....";
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new System.Drawing.Point(81, 47);
-            textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            textBox2.MaxLength = 15;
-            textBox2.Name = "textBox2";
-            textBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            textBox2.Size = new System.Drawing.Size(121, 23);
-            textBox2.TabIndex = 4;
-            // 
-            // textBox3
-            // 
-            textBox3.Location = new System.Drawing.Point(226, 47);
-            textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            textBox3.MaxLength = 15;
-            textBox3.Name = "textBox3";
-            textBox3.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            textBox3.Size = new System.Drawing.Size(121, 23);
-            textBox3.TabIndex = 6;
             // 
             // label3
             // 
@@ -114,16 +94,6 @@
             label3.Text = "User ID";
             label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // textBox4
-            // 
-            textBox4.Location = new System.Drawing.Point(367, 47);
-            textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            textBox4.MaxLength = 15;
-            textBox4.Name = "textBox4";
-            textBox4.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            textBox4.Size = new System.Drawing.Size(121, 23);
-            textBox4.TabIndex = 8;
-            // 
             // label4
             // 
             label4.AutoSize = true;
@@ -136,21 +106,45 @@
             label4.Text = "Password";
             label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // systemip
+            // 
+            systemip.Location = new System.Drawing.Point(81, 47);
+            systemip.Name = "systemip";
+            systemip.Size = new System.Drawing.Size(97, 23);
+            systemip.TabIndex = 10;
+            systemip.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // userid
+            // 
+            userid.Location = new System.Drawing.Point(226, 47);
+            userid.Name = "userid";
+            userid.Size = new System.Drawing.Size(97, 23);
+            userid.TabIndex = 11;
+            userid.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // userpw
+            // 
+            userpw.Location = new System.Drawing.Point(367, 47);
+            userpw.Name = "userpw";
+            userpw.Size = new System.Drawing.Size(97, 23);
+            userpw.TabIndex = 12;
+            userpw.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             ClientSize = new System.Drawing.Size(700, 450);
-            Controls.Add(textBox4);
+            Controls.Add(userpw);
+            Controls.Add(userid);
+            Controls.Add(systemip);
             Controls.Add(label4);
-            Controls.Add(textBox3);
             Controls.Add(label3);
-            Controls.Add(textBox2);
             Controls.Add(textBox1);
-            Controls.Add(label2);
+            Controls.Add(cmdstr);
             Controls.Add(label1);
             Controls.Add(button1);
-            Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            Margin = new System.Windows.Forms.Padding(2);
             Name = "Form1";
             ResumeLayout(false);
             PerformLayout();
@@ -159,13 +153,13 @@
         #endregion
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label cmdstr;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox systemip;
+        private System.Windows.Forms.MaskedTextBox userid;
+        private System.Windows.Forms.MaskedTextBox userpw;
     }
 }
 
--- .Net framework/myproject8/Form1.cs
+++ .Net framework/myproject8/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using com.ibm.as400.access;
@@ -21,22 +22,62 @@ namespace myproject8
 
         private void button1_Click(object sender, EventArgs e)
         {
-            AS400 as400 = new AS400("172.16.14.61", "QSECOFR", "PASSWORD");
-            CommandCall cmd = new CommandCall(as400);
+            string conn_sysip = systemip.Text;  // Get System IP Address
+            string conn_userid = userid.Text;   // Get System User ID
+            char[] conn_userpw = userpw.Text.ToCharArray();  // Get System User Password
+
             try
             {
+                // validate connect System IP
+                string pattern = @"\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b";
+                if (Regex.IsMatch(conn_sysip, pattern))
+                {
+                    systemip.Text = conn_sysip;
+                }
+                else
+                {
+                    cmdstr.Text = "IP addr is not correct.";
+                    return;
+                }
+
+                // validate connect System User ID
+                if (conn_userid.Length <= 8)
+                {
+                    userid.Text = conn_userid;
+                }
+                else
+                {
+                    cmdstr.Text = "User Id length is not correct.";
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+                return;
+            }
+
+            try
+            {
+                AS400 as400 = new(conn_sysip, conn_userid, conn_userpw);
+                CommandCall cmd = new CommandCall(as400);
+
                 // Run the command.
                 if (cmd.run("CRTLIB TBXSAMPLE"))
-                    label1.Text = cmd.getCommand();
+                {
+                    cmdstr.Text = cmd.getCommand() + " executed successfully.";
+                }
                 else
-                    label1.Text = "Command failed";
+                {
+                    cmdstr.Text = "Command failed";
+                }
 
                 // If messages were produced from the command, print them
                 AS400Message[] messagelist = cmd.getMessageList();
 
                 if (messagelist.Length > 0)
                 {
-                    label2.Text = "messages from the command:";
+                    cmdstr.Text = "messages from the command:";
                 }
 
                 for (int i = 0; i < messagelist.Length; i++)
@@ -44,9 +85,13 @@ namespace myproject8
                     textBox1.Text = messagelist[i].getID() + ":" + messagelist[i].getText();
                 }
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-            };
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            systemip.Clear();
+            userid.Clear();
+            userpw.Clear();
         }
     }
 }
