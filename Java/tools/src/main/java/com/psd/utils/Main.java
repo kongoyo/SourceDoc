eaf19d49bda516b4ceabcafdbc585a6868dade76
kongoyo <87678893+kongoyo@users.noreply.github.com>
2025-01-15 17:58:41 +0800

    modified:   Java/tools/src/main/java/com/psd/utils/Main.java 	modified:   Java/tools/target/classes/com/psd/utils/Main.class

--- Java/tools/src/main/java/com/psd/utils/Main.java
+++ Java/tools/src/main/java/com/psd/utils/Main.java
@@ -2,24 +2,40 @@ package com.psd.utils;
 
 import com.ibm.as400.access.AS400;
 import com.ibm.as400.access.CommandCall;
+import com.ibm.as400.access.PrintObjectInputStream;
+import com.ibm.as400.access.SpooledFile;
 
 public class Main 
 {
     public static void main( String[] args )
     {
         try {
-            Class.forName("com.ibm.as400.access.AS400JDBCDriver");
-            System.out.println("AS400JDBCDriver loaded!");
+            // Class.forName("com.ibm.as400.access.AS400JDBCDriver");
+            // System.out.println("AS400JDBCDriver loaded!");
             String system = "172.16.13.58";
             String usr = "ibmecs";
             String pwd = "ibmecsusr";
-            String cmd = "WRKACTJOB";
+            String cmd = "WRKACTJOB OUTPUT(*PRINT)";
+
+            // Login
             AS400 sys = new AS400(system, usr, pwd);
+            
+            // Execute command
             CommandCall cmdcall = new CommandCall(sys);
             cmdcall.run(cmd);
-            System.out.println(cmdcall.getClass());
+            
+            // Retrieve Job
+            System.out.println(cmdcall.getServerJob());
+
+            // Retrieve Job Splf 
+            SpooledFile splf = new SpooledFile(sys, system, 0, pwd, usr, cmd);
+            System.out.println(splf);
+            String splfjob = splf.getJobName();
+            PrintObjectInputStream splfinp = splf.getInputStream();
+            // SpooledFileViewer splf = new SpooledFileViewer(splfout);
+            System.out.println(splfinp);
         } catch (Exception e) {
-            System.out.println("JDBC Driver Not Found!");
+            System.out.println(e);
         }
     }
 }
