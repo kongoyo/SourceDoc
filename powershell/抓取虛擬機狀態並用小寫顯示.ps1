4a0b00dedefd8ab69e88881a89101a46c9d62312
kongoyo <kongoyo@gmail.com>
2025-09-10 14:11:21 +0800

    modified:   .Net framework/myproject8/obj/project.nuget.cache 	modified:   .gitignore

---
+++ powershell/抓取虛擬機狀態並用小寫顯示.ps1
@@ -0,0 +1,14 @@
+﻿# 嘗試取得 HMC 虛擬主機，如果不存在則 vm 會是 null
+$vm = Get-VM -Name HMC -ErrorAction SilentlyContinue
+
+# 檢查 $vm 是否為 null
+if ($vm -eq $null) {
+    $ps = 'Off'
+} else {
+    $ps = [string]$vm.State
+}
+
+$psLow = $ps.ToLower()
+
+# 驗證 $ps 的值
+Write-Host "The value of `$ps` is: $psLow"
